02bac70bea9503f536a7e1b170b2c2e3eece882c
somin <somin3415@naver.com>
2021-08-18 20:21:52 +0900

    collapsing header

---
+++ CollapsingHeader2/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CollapsingHeader
+//
+//  Created by 신소민 on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var headerHeight: NSLayoutConstraint!
+    @IBOutlet weak var scrollViewTop: NSLayoutConstraint!
+    
+    private let maxHeaderHeight: CGFloat = 250
+    private let minHeaderHeight: CGFloat = 80
+    private var previousScrollOffset: CGFloat = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set scrollView
+        scrollViewTop.constant = minHeaderHeight
+        scrollView.contentInset.top = maxHeaderHeight - minHeaderHeight
+        
+        // set headerview height
+        headerHeight.constant = maxHeaderHeight
+    }
+
+}
